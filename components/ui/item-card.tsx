673380c878b6daabf5e7d0417f459ae3ac5dbf16
fishnchipa <wilsoncaox@gmail.com>
2024-01-23 22:51:46 +1100

    item card component

--- components/ui/item-card.tsx
+++ components/ui/item-card.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import AddToCart from './add-to-cart'
+import { ItemType } from '@/lib/types'
 
 
-const ItemCard = () => {
+const ItemCard = ({id, name, price, category}: ItemType) => {
+	
 	return (
 		<div className="flex flex-col justify-between w-[250px] border-gray-300 border-[1px] p-2">
 			<div className="w-full">
 				<img src="/acid.jpeg" alt="item" className="object-fit h-[200px] w-full"/>
 			</div>
 			<div>
-				<h1>Vintage Album</h1>
-				<h2>$99</h2>
+				<h1>{name}</h1>
+				<h2>${price}</h2>
+				<h3>{id}</h3>
 			</div>
 			<div className="flex flex-row justify-end">
-				<AddToCart />
+				<AddToCart itemId={id}/>
 			</div>
 
 
