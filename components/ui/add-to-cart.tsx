3d0ced371c47a11c3b07c9648be3e32163994c58
fishnchipa <wilsoncaox@gmail.com>
2024-01-23 22:51:34 +1100

    added cart functionality

--- components/ui/add-to-cart.tsx
+++ components/ui/add-to-cart.tsx
@@ -1,8 +1,33 @@
+"use client"
+
+import axios from 'axios'
+import { DefaultSession } from 'next-auth'
+import { useSession } from 'next-auth/react'
 import React from 'react'
 
-const AddToCart = () => {
+interface User extends DefaultSession {
+	id: string
+}
+
+const AddToCart = ({itemId}: {itemId: number}) => {
+	const { data: session, status } = useSession()
+
+	const user = session?.user as User
+	const addToCart = () => {
+		axios.post("http://localhost:8080/cart/add", {
+			id: {
+				user: user.id,
+				item: itemId
+			}
+		})
+		
+	}
+
 	return (
-		<button className="bg-slate-900 rounded-3xl py-2 px-4 text-white">
+		<button 
+			className="bg-slate-900 rounded-3xl py-2 px-4 text-white hover:bg-slate-700" 
+			onClick={addToCart}
+		>
 			Add to Cart
 		</button>
 	)
