9fcffbffa13bd77eb4a941fe39f478cef364ce11
fishnchipa <wilsoncaox@gmail.com>
2024-01-30 22:21:59 +1100

    resize banner image

--- components/ui/banner.tsx
+++ components/ui/banner.tsx
@@ -1,14 +1,22 @@
+"use client"
+
 
 import { BannerProps } from '@/lib/types'
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React, { useEffect, useState } from 'react'
 
-const Banner = ({width, height, image}: BannerProps) => {
-
+const Banner = ({image}: {image: string}) => {
 
 	return (
-		<div className="rounded-md overflow-hidden hidden lg:block">
-			<Image src={image}  alt="banner" width={900} height={400}/>
+		<div className="flex flex-row justify-between items-center bg-red-500 p-[10rem] gap-x-[10rem] h-full w-full">
+			<div className="flex flex-col items-center ">
+				<h1 className="text-2xl font-semibold">Discover Your New Ride!</h1>
+				<p className="text-xs">Search through our ranges of different offers</p>
+			</div>
+			<div className="relative h-[100px] w-[100px]">
+				
+				<Image src={image} alt="banner" fill={true}/>
+			</div>
 		</div>
 	)
 }
