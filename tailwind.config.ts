0d4ef046eaa1caa7a7e2597f4fe9ee0d1bc3f73b
fishnchipa <wilsoncaox@gmail.com>
2024-02-01 20:26:09 +1100

    custom responsive layout and animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,8 +16,13 @@ const config = {
       screens: {
         "2xl": "1400px",
       },
+
     },
     extend: {
+      screens: {
+        "first" : "1168px",
+        "second" : "1200px",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -67,10 +72,12 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+
       },
     },
   },
