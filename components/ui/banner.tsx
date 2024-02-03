278e905e848cbd52ee7c71e0f6b184b107e22aff
fishnchipa <wilsoncaox@gmail.com>
2024-02-03 18:45:41 +1100

    banner responsive

--- components/ui/banner.tsx
+++ components/ui/banner.tsx
@@ -8,14 +8,21 @@ import React, { useEffect, useState } from 'react'
 const Banner = ({image}: {image: string}) => {
 
 	return (
-		<div className="flex flex-row justify-between items-center bg-red-500 p-[10rem] gap-x-[10rem] h-full w-full">
+		<div 
+			className="flex flex-col items-center px-[1rem] gap-x-[10rem] h-full w-full gap-y-[10rem] z-[-1]
+			mt-[6rem]
+			mobile:flex-row mobile:justify-between mobile:px-[20%]  mobile:mt-[0]
+			
+			">
 			<div className="flex flex-col items-center ">
 				<h1 className="text-2xl font-semibold">Discover Your New Ride!</h1>
 				<p className="text-xs">Search through our ranges of different offers</p>
 			</div>
-			<div className="relative h-[100px] w-[100px]">
+			<div 
+				className="relative w-[20%] box-border px-[30%] py-[20%]"
+			>
 				
-				<Image src={image} alt="banner" fill={true}/>
+				<Image src={image} fill alt="banner" />
 			</div>
 		</div>
 	)
